40f1b705dd67294d333a23dab402003ac7fb956d
Danny Lin <danny@kdrag0n.dev>
2021-05-14 17:37:55 -0700

    index: Update hero button

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/install/web">
+            Install
           </Link>
         </div>
       </div>
