2414a197eae6c0cfec2f32c64f43d453b36bd8cd
Danny Lin <danny@kdrag0n.dev>
2021-05-16 02:08:47 -0700

    screenshots: Add captions to screenshots

--- src/components/Screenshot.js
+++ src/components/Screenshot.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import clsx from 'clsx';
+import styles from './Screenshots.module.css';
 
 export default function Screenshot({name, alt}) {
   return (
-    <picture>
-      <source type="image/webp" srcset={`../../static/img/screenshots/${name}.webp`} />
-      <source type="image/png" srcset={`../../static/img/screenshots/${name}.png`} />
-      <img width="205" height="433" src={`../../static/img/screenshots/${name}.png`} alt={alt} />
-    </picture>
+    <div className={clsx(styles.screenshotContainer)}>
+      <picture>
+        <source type="image/webp" srcset={`../../static/img/screenshots/${name}.webp`} />
+        <source type="image/png" srcset={`../../static/img/screenshots/${name}.png`} />
+        <img width="205" height="433" src={`../../static/img/screenshots/${name}.png`} alt={alt} />
+      </picture>
+      <br />
+      <span className={clsx(styles.screenshotCaption)}>{alt}</span>
+    </div>
   );
 }
