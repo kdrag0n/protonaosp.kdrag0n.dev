4d200c14f513b538093ed55a90609c89ecd5296c
Danny Lin <danny@kdrag0n.dev>
2021-06-17 18:25:23 -0700

    Override release index for ProtonAOSP 11.5.1
    This removes the need to keep pushing updates to android-webinstall.

--- build.sh
+++ build.sh
@@ -20,8 +20,11 @@ popd
 # Combine built sites
 mv android-webinstall/dist build/install/_web
 
-# Relocate release index
+# Relocate /releases/
 mv build/install/_web/releases build/
+
+# Override release index with ProtonAOSP version
+cp -f releases.json build/releases/index.json
 
 # Correct paths for trailingSlash=false
 cp -r build/install/_web/js build/install/
