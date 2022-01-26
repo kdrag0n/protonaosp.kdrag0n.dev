b940503fb28f1ace7ecfa2deb6612ee6b71af8f8
Danny Lin <danny@kdrag0n.dev>
2022-01-25 20:11:42 -0800

    versions: Release 12.2.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -163,12 +163,10 @@ module.exports = {
     colorMode: {
       respectPrefersColorScheme: true,
     },
-    /*
     announcementBar: {
-      id: 'release_12.2.0-test1',
-      content: '<a href="/versions/12.2.0?utm_source=docs-banner&utm_campaign=12.2.0-banner">ProtonAOSP 12.2.0 is now available in early access!</a>',
+      id: 'release_12.2.1-test1',
+      content: '<a href="/versions/12.2.1?utm_source=docs-banner&utm_campaign=12.2.1-banner">ProtonAOSP 12.2.1 is now available in early access!</a>',
     },
-    */
     algolia: process.env.ALGOLIA_API_KEY !== undefined ? {
       appId: process.env.ALGOLIA_APP_ID,
       apiKey: process.env.ALGOLIA_API_KEY,
